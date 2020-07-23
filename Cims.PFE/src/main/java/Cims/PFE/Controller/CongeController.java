e3fc0f824f4a8bb9d98e16fffa67ad6449af1d90
lasoued.iheb@gmail.com <Iheb.lassoued@esprit.tn>
2020-07-23 18:05:58 +0100

    modification au niveau methode ajouterConge

--- Cims.PFE/src/main/java/Cims/PFE/Controller/CongeController.java
+++ Cims.PFE/src/main/java/Cims/PFE/Controller/CongeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ import Cims.PFE.Entities.Conge;
 import Cims.PFE.Entities.Grade;
 import Cims.PFE.Service.CongeService;
 import Cims.PFE.payload.response.MessageResponse;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class CongeController {
@@ -41,10 +42,10 @@ public class CongeController {
 		congeService.demanderConge(c, personnel_id);
 		return ResponseEntity.status(HttpStatus.OK).body(new MessageResponse("demande ajouter"));
 	}
-	@PostMapping(value = "/ajouterConge/{matricule}")
+	@PostMapping(value = "/ajouterConge/{personnel_id}")
 	public ResponseEntity<MessageResponse>ajouterConge(@RequestBody Conge c,
-			@PathVariable("matricule") int matricule) {
-		congeService.ajouterConge(c, matricule);
+			@PathVariable("personnel_id") long id) {
+		congeService.ajouterConge(c, id);
 		return ResponseEntity.status(HttpStatus.OK).body(new MessageResponse("demande ajouter"));
 	}
 	

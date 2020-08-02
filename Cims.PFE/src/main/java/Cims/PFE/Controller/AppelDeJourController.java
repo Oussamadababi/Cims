617496110eec7a689d5ac714c1c7e4b47f571a55
lasoued.iheb@gmail.com <Iheb.lassoued@esprit.tn>
2020-08-02 01:07:56 +0100

    appeldejour

--- Cims.PFE/src/main/java/Cims/PFE/Controller/AppelDeJourController.java
+++ Cims.PFE/src/main/java/Cims/PFE/Controller/AppelDeJourController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import Cims.PFE.Entities.Personnel;
 import Cims.PFE.Service.AppelDeJourService;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class AppelDeJourController {

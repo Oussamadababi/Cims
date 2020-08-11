4ea0a201dd4c30cbc58a279292939f906cc811ad
Oussama Dababi <oussama.dababi@esprit.tn>
2020-08-11 01:19:44 +0100

    Affichage Demande Recuperation SR

--- Cims.PFE/src/main/java/Cims/PFE/Controller/RecuperationSoldeReposController.java
+++ Cims.PFE/src/main/java/Cims/PFE/Controller/RecuperationSoldeReposController.java
@@ -1,15 +1,19 @@
 package Cims.PFE.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import Cims.PFE.Entities.Conge;
 import Cims.PFE.Entities.RecuperationSoldeRepos;
 import Cims.PFE.Service.RecuperationSoldeReposService;
 import Cims.PFE.payload.response.MessageResponse;
@@ -27,6 +31,10 @@ public class RecuperationSoldeReposController {
 		recuperationSoldeRepos.ajouterdemandeRSR(c,idPersonnel);
 		return ResponseEntity.status(HttpStatus.OK).body(new MessageResponse("Demande RSR ajouter"));
 
+	}
+	@GetMapping(value = "/listRSR")
+	public List<RecuperationSoldeRepos> listRSR() {
+		return recuperationSoldeRepos.listAll();
 	}
 
 }

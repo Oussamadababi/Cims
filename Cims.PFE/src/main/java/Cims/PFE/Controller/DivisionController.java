e7398eb81d574f40369a7e5700ba76e1878b4a1c
lasoued.iheb@gmail.com <Iheb.lassoued@esprit.tn>
2020-08-26 14:37:57 +0100

    division

--- Cims.PFE/src/main/java/Cims/PFE/Controller/DivisionController.java
+++ Cims.PFE/src/main/java/Cims/PFE/Controller/DivisionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import Cims.PFE.Entities.Division;
 import Cims.PFE.Entities.Structure;
 import Cims.PFE.Service.DivisionService;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class DivisionController {
--- Cims.PFE/src/main/java/Cims/PFE/Service/DivisionService.java
+++ Cims.PFE/src/main/java/Cims/PFE/Service/DivisionService.java
@@ -1,5 +1,6 @@
 package Cims.PFE.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import Cims.PFE.Dao.DivisionRepository;
 import Cims.PFE.Entities.Division;
+import Cims.PFE.Entities.Grade;
 
 @Service
 public class DivisionService {
@@ -15,7 +17,9 @@ public class DivisionService {
 
 	public List<Division> getDivisionParStructure( long idStructure)
 	{
-		return divisionRepository.getDivisionParStructure(idStructure);
+		List<Division> Divisions = new ArrayList<>();
+		divisionRepository.getDivisionParStructure(idStructure).forEach(Divisions::add);
+		return Divisions;
 	}
 
 }

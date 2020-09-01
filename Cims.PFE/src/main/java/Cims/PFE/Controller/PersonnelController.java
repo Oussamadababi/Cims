d33418e156091c6ac212c0e0840704350c4a25c2
lasoued.iheb@gmail.com <Iheb.lassoued@esprit.tn>
2020-09-02 00:01:03 +0100

    personnel

--- Cims.PFE/src/main/java/Cims/PFE/Controller/PersonnelController.java
+++ Cims.PFE/src/main/java/Cims/PFE/Controller/PersonnelController.java
@@ -120,8 +120,8 @@ public class PersonnelController {
 	}
 	
 	
-	@PostMapping("/addPersonnel")
-	public ResponseEntity<?> registerUser(@RequestBody Personnel p ,SignupRequest signUpRequest) {
+	@PostMapping("/addPersonnel/{idFonction}/{idStructure}/{idAffectation}")
+	public ResponseEntity<?> registerUser(@RequestBody Personnel p ,SignupRequest signUpRequest,@PathVariable(name="idFonction") Long idFonction, @PathVariable(name="idStructure") Long idStructure,@PathVariable(name="idAffectation") Long idAffectation) {
 		signUpRequest.setUsername(p.getPrenom()+"."+p.getNom());
 		
 				
@@ -149,7 +149,7 @@ public class PersonnelController {
 			});
 		}
 		if(personnelRepository.existsByEmail(p.getEmail())==false) {
-		if( personnelService.save(p)!= null) {
+		if( personnelService.save2(p,idFonction,idStructure,idAffectation)!= null) {
 			compte.setRoles(roles);
 			p.setUser(compte);
 			compte.setPersonnel(p);

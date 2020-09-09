c37974acb906431cb53cb6772f4747c5455560b5
Oussama Dababi <oussama.dababi@esprit.tn>
2020-09-09 15:03:18 +0100

    Commit

--- Cims.PFE/src/main/java/Cims/PFE/Dao/AnnulationCongeRepository.java
+++ Cims.PFE/src/main/java/Cims/PFE/Dao/AnnulationCongeRepository.java
@@ -1,14 +1,11 @@
 package Cims.PFE.Dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import Cims.PFE.Entities.AnnulationConge;
-import Cims.PFE.Entities.Conge;
 
 
 @Repository
@@ -18,7 +15,7 @@ public interface AnnulationCongeRepository extends JpaRepository<AnnulationConge
 	public void ModifierEtatAnulationConge(String etat,long id);
 	@Query(value = "SELECT *FROM public.annulation_conge a join conge c on a.conge_id=c.id where c.personnel_id=?1 and a.etat='En_attente'", nativeQuery = true)
 	public AnnulationConge AnulationCongeenattente(long idPersonnel);
-	@Query(value = "SELECT id FROM public.annulation_conge  where conge_id=?1", nativeQuery = true)
-	public  Object AnulationConge(long idPersonnel);
+	@Query(value = "SELECT * FROM public.annulation_conge  where conge_id=?1", nativeQuery = true)
+	public  AnnulationConge AnulationConge(long idPersonnel);
 
 }

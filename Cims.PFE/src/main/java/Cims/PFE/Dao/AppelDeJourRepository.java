6e6f8962caee0c5906417119497d0d81ab5f1f8c
lasoued.iheb@gmail.com <Iheb.lassoued@esprit.tn>
2020-10-04 14:26:11 +0100

    listAbsenceSansJusftication

--- Cims.PFE/src/main/java/Cims/PFE/Dao/AppelDeJourRepository.java
+++ Cims.PFE/src/main/java/Cims/PFE/Dao/AppelDeJourRepository.java
@@ -2,11 +2,13 @@ package Cims.PFE.Dao;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import Cims.PFE.Entities.Absence;
 import Cims.PFE.Entities.AppelDeJour;
 import Cims.PFE.Entities.Pesonnel_Absent_SJ;
 
@@ -31,10 +33,8 @@ public interface AppelDeJourRepository extends JpaRepository<AppelDeJour, Date> 
 	@Query(value = "SELECT COUNT(*)FROM appel_de_jour_personnels c where personnels_id_personnel=?1 and EXTRACT(YEAR FROM NOW())-EXTRACT(YEAR FROM c.appels_datedujour)<=2", nativeQuery = true)
 	public  int NbAbscenceparId(Long id);
 	
-//	@Query(value = "SELECT * from appel_de_jour_personnels c join personnel p on c.personnels_id_personnel=p.id_personnel join appel_de_jour a on c.appels_datedujour=a.datedujour where a.etat='Sansjusitf'", nativeQuery = true)
-//	public  List<Personnel> ListeAbsenceSansJustifiaction();
-	@Query(value = "SELECT p.nom,p.prenom,p.nom_AR,p.prenom_AR,p.poste_occupe,p.matricule,a.datedujour from appel_de_jour_personnels c join c.personnel p on c.personnels_id_personnel=p.id_personnel join c.appel_de_jour a on c.appels_datedujour=a.datedujour where a.etat='Sansjusitf' ", nativeQuery = true)
-	public  ArrayList<Object> ListeAbsenceSansJustifiaction2();
+	@Query(value = "SELECT NEW Cims.PFE.Entities.Absence (p.nom,p.prenom,a.datedujour) FROM AppelJourPersonnel  ap join ap.personnels p join ap.appelDeJour a where  ap.etat='Sansjusitf' order by  a.datedujour")
+	List<Absence> listAbsenceSansJusftication();
 	
 
 }
